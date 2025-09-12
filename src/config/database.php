9eb0e7c606ef69d079706e3ca906cd1dc4aed1f5
Paolo Paz <ppaz@clinicapj.org.pe>
2025-09-12 16:08:30 -0500

    feat: configurar conexión MongoDB en config/database.php

--- src/config/database.php
+++ src/config/database.php
@@ -93,6 +93,18 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGO_DB_HOST', '127.0.0.1'),
+            'port'     => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE'),
+            'username' => env('MONGO_DB_USERNAME'),
+            'password' => env('MONGO_DB_PASSWORD'),
+            'options'  => [
+                'database' => env('MONGO_DB_AUTHENTICATION_DATABASE', 'admin'), // base de autenticación
+            ],
+        ],
+
     ],
 
     /*
